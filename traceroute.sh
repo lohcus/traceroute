12a54b3a376ff43f1447ac32d35a13f756d9c2ae
lohcus <49207591+lohcus@users.noreply.github.com>
2020-07-06 22:17:41 -0300

    Update traceroute.sh

--- traceroute.sh
+++ traceroute.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Criado por Daniel Domingues
-#Versao 1.1
-#https://github.com/lohcus
-
 colunas=$(tput cols) # VERIFICA O TAMANHO DA JANELA PARA PODER DESENHAR O LAYOUT
 
 divisao () {
@@ -18,6 +14,7 @@ divisao () {
 }
 # ================================================================================
 
+# TESTA SE FOI DIGITADO O DOMÍNIO
 if [ -z "$1" ]
 then
 	echo "Uso: $0 URL"
@@ -25,9 +22,19 @@ then
 fi
 
 clear
+
+# VARIÁVEIS PARA FACILITAR A ESCRITA NA TELA
 vermelho="\033[31;1m"
 verde="\033[32;1m"
-branco="\033[m"
+branco="\033[37;1m"
+
+divisao
+
+centro_coluna=$(( $(( $(( $colunas-17))/2 )))) #CALCULO PARA CENTRALIZAR O TEXTO
+tput cup 0 $centro_coluna #POSICIONAR O CURSOR
+printf "\033[37;1mSCRIPT TRACEROUTE\n\033[m"
+
+# TESTE PARA VERIFICAR SE O DOMÍNIO RESPONDE
 teste=$(host $1 | grep "not found")
 if [ ! -z "$teste" ]
 then
@@ -35,37 +42,41 @@ then
 	exit 1
 fi
 
-printf "$vermelho[+] Fazendo traceroute para o endereço $verde$1\n$branco"
+printf "$branco[+] Fazendo traceroute para o endereço $verde$1\n$branco"
 divisao
 
 for i in $(seq 1 255)
 do
 	#FAZ UM PING PARA O DOMINIO OU ENDERECO DIGITADO COM UM TTL=$i
-	pingo=$(ping -4 -c 1 -w 1 -t $i $1 2> /dev/null)
+	retorno=$(ping -4 -c 1 -w 1 -t $i $1 2> /dev/null)
 
 	#TESTA SE FOI RETORNADA A MENSAGEM ICMP "Time to live exceeded"
-	if [[ $(echo $pingo | grep "From" | cut -d "=" -f 2 | cut -d " " -f 2-5) == "Time to live exceeded" ]]
+	if [[ $(echo $retorno | grep "From" | cut -d "=" -f 2 | cut -d " " -f 2-5) == "Time to live exceeded" ]]
 	then
-		if [[ $(echo $pingo | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1) == "" ]] #CASO NAO CONSIGA SABER O IP DO ROTEADOR, IMPRIME UM "*" VERDE
+		if [[ $(echo $retorno | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1) == "" ]] #CASO NAO CONSIGA SABER O IP DO ROTEADOR, IMPRIME UM "*" VERDE
 		then
 			printf "$verde$i - *\n$branco"
 		else
-			pingo=$(echo $pingo | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1) #SALVA O IP DO ROTEADOR QUE RETORNO A MENSAGEM ICMP "Time to live exceeded"
-			printf "$verde$i - $pingo\n$branco"
+			retorno=$(echo $retorno | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -1) #SALVA O IP DO ROTEADOR QUE RETORNOU A MENSAGEM ICMP "Time to live exceeded"
+			printf "$verde$i - $retorno\n$branco"
 		fi
 	else #CASO NEGATIVO
-		if [[ $(echo $pingo | grep "ttl=" | cut -d "=" -f 3 | cut -d " " -f 1) ]]
+		if [[ $(echo $retorno | grep "ttl=" | cut -d "=" -f 3 | cut -d " " -f 1) ]]
 		then
 			#SALVA O IP DO ALVO E O TTL DA CONEXAO
-			ip=$(echo $pingo | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -2)
-			pingo=$(echo $pingo | grep "ttl=" | cut -d "=" -f 3 | cut -d " " -f 1)
+			ip=$(echo $retorno | cut -d " " -f 9- | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | head -2)
+			retorno=$(echo $retorno | grep "ttl=" | cut -d "=" -f 3 | cut -d " " -f 1)
 			divisao
+			
 			#TESTE PARA FAZER UMA SUGESTAO DO SISTEMA OPERACIONAL DO ALVO
-			case $pingo in
-				[0-9]|[0-5][0-9]|6[0-4]) printf "\033[32;1m[+] DESTINO ALCANÇADO! \033[31;1m$ip\033[32;1m: TTL foi \033[31;1m$pingo\033[32;1m, ou seja, \033[31;1m64\033[32;1m decrementado de \033[31;1m$((64-$pingo))\033[32;1m (\033[31;1mLinux\033[32;1m)\n\033[m" ;;
-				6[5-9]|[7-9][0-9]|1[0-1][0-9]|12[0-8])printf "\033[32;1m[+] DESTINO ALCANÇADO! \033[31;1m$ip\033[32;1m: TTL foi \033[31;1m$pingo\033[32;1m, ou seja, \033[31;1m64\033[32;1m decrementado de \033[31;1m$((128-$pingo))\033[32;1m (\033[31;1mWindows\033[32;1m)\n\033[m" ;;
-				12[8-9]|1[3-9][0-9]|2[0-5][0-9]|25[0-5])printf "\033[32;1m[+] DESTINO ALCANÇADO! \033[31;1m$ip\033[32;1m: TTL foi \033[31;1m$pingo\033[32;1m, ou seja, \033[31;1m64\033[32;1m decrementado de \033[31;1m$((255-$pingo))\033[32;1m (\033[31;1mUnix\033[32;1m)\n\033[m" ;;
+			case $retorno in
+				[0-9]|[0-5][0-9]|6[0-4]) destino="Linux"; dec=64 ;;
+				6[5-9]|[7-9][0-9]|1[0-1][0-9]|12[0-8]) destino="Windows"; dec=128 ;;
+				12[8-9]|1[3-9][0-9]|2[0-5][0-9]|25[0-5]) destino="Unix"; dec=255 ;;
 			esac
+			
+			printf "\033[32;1m[+] DESTINO ALCANÇADO! \033[31;1m$ip\033[32;1m: TTL foi \033[31;1m$retorno\033[32;1m, ou seja, \033[31;1m64\033[32;1m decrementado de \033[31;1m$(($dec-$retorno))\033[32;1m (\033[31;1mpossivelmente $destino\033[32;1m)\n\033[m"
+
 			divisao
 			#ASSIM QUE ATINGE O ALVO, O SCRIPT E ENCERRADO
 			exit 0
